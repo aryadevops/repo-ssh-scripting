61d50c8f88912fb0b617671e1609613fc9273be3
Arun Kumar <info@aryadevops.com>
2024-03-03 13:10:18 +0530

    Arrey functionality testing

--- 06-array.sh
+++ 06-array.sh
@@ -2,8 +2,8 @@
 
 PERSONS=("Raj" "Ram" "Ravi")
 
-echo "getting First name is $PERSONS{[0]}"
+echo "getting First name is ${PERSONS[0]}"
 
-echo "getting First name is $PERSONS{[1]}"
+echo "getting First name is ${PERSONS[1]}"
 
-echo "getting First name is $PERSONS{[2]}"
+echo "getting First name is ${PERSONS[2]}"
