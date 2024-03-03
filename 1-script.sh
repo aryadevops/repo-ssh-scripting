d1b96ae06bd55e2cd4db3142ce6d9bc8429abb7c
Arun Kumar <info@aryadevops.com>
2024-03-03 12:16:11 +0530

    basic conversation

---
+++ 1-script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ram
+PERSON2=Raj
+
+cat "$PERSON1 : Hi $PERSON2, How are you!"
+cat "$PERSON2 : Hi $PERSON1, Im good.u?"
+cat "$PERSON1 : im good $PERSON2,wt u do $PERSON2"
+cat "$PERSON2 : Nothing $PERSON1"
