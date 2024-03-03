5ae3e7034ee35e1852d5aa62409bb1ae9592bdd0
Arun Kumar <info@aryadevops.com>
2024-03-03 17:17:41 +0530

    root user access testing with exit command

--- 07-root-access.sh
+++ 07-root-access.sh
@@ -6,6 +6,7 @@ if [ $USER -ne 0 ]
 then
 
     echo "Error :: The User is not a root user, u can run this command with root user"
+    exit 1 ## we can confirm here to exit from the program.
  fi   
 
  yum install git -y
