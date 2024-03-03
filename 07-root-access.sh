f46bcebe2be6c130f3d55e4b74ad8d5679bbea98
Arun Kumar <info@aryadevops.com>
2024-03-03 17:14:14 +0530

    root user access testing

--- 07-root-access.sh
+++ 07-root-access.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-USER=$( id -u )
+USER=$(id -u)
 
-echo "User Id  is : $USER"
+if [ $USER -ne 0 ]
+then
+
+    echo "Error :: The User is not a root user, u can run this command with root user"
+ fi   
+
+ yum install git -y
