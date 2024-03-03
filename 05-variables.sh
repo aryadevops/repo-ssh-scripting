11e5cccb192482e8926f8fb1979ac5776c37e587
Arun Kumar <info@aryadevops.com>
2024-03-03 12:45:42 +0530

    User name and Password

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Enter your User name:"
+
+read USERNAME
+
+echo "Enter your Password"
+read PASSWORD
+
+echo "Entered User Name : $USERNAME "
+echo " Entered Password : $PASSWORD"
