95b363d6c69a2960e0b49009a64df760c46bbbf7
Arun Kumar <info@aryadevops.com>
2024-03-04 23:57:36 +0530

    looping installation with colors

--- 12-loops.sh
+++ 12-loops.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+B="\e[34m"
 
 USERID=$(id -u)
-NUMIN=$($#) ## Number of input installations
+##NUMIN=$($#) ## Number of input installations
 
 
 if [ $USERID -ne 0 ]
@@ -16,20 +20,20 @@ VALIDATE(){
 
     if [$1 -ne 0]
     then 
-        echo "Installation Failure"
+        echo -e "Installation $R Failure $N"
      else
-        echo "Installation Success"
+        echo -e "Installation $G Success $N"
     fi       
 }
  
  for i in $@
  do 
-    yum install $i -y
+    yum install $i -y >>File.log
 
 ## for i in {"git" "postfix"}
  ## do
  ## yum install $i -y
  VALIDATE $? $i
 done
-echo "Totall number of packages $NUMIN"
-echo "The program $0 is successfully completed"
+echo "Totall number of packages $#"
+echo -e "The program $B $0 $N is $G successfully completed $G"
