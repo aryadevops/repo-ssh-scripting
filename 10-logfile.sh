cd16610a441d8f70cca565d49559e11d7c500ccf
Arun Kumar <info@aryadevops.com>
2024-03-04 15:56:29 +0530

    testing logfiles

---
+++ 10-logfile.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+
+VALIDATE(){
+
+if [ $1 -ne 0 ]
+ then
+    echo "$2 installation failure..."
+else 
+    echo "$2 Installation sucees...."   
+fi
+
+}
+
+USERID=$(id -u)
+
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this commond with root access"
+    exit 1
+fi
+
+yum install git -y
+
+VALIDATE $? "Git" &>>file.log
+
+yum install postfix -y
+
+VALIDATE $?  "Postfix"   
